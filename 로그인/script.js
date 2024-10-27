de91bba15f868c001e927b4f13530c74a56bfb7d
sxs <69sxs74@gmail.com>
2024-10-27 18:55:23 +0900

    Update script.js

--- 로그인/script.js
+++ 로그인/script.js
@@ -3,7 +3,7 @@
         var u = document.getElementById("username").value;
         var p = document.getElementById("password").value;
         if (u === "test" && p === "test") {
-            window.location.href = "../sxs.html"; // 상위 디렉토리로 이동
+            window.location.href = "../sxs/index.html"; // 상위 디렉토리로 이동
         } else {
             alert("아이디 또는 비밀번호가 틀렸습니다.");
         }
